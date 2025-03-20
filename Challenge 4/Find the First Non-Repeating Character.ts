7c70b16e81dd3ff2f3f3b66a78ecebab1cb23b19
gilbert sahyoun <gilbertsahyoun.business@gmail.com>
2025-03-20 15:43:02 +0200

    Add Challenge 4 Sol

---
+++ Challenge 4/Find the First Non-Repeating Character.ts
@@ -0,0 +1,6 @@
+function firstNonRepeatingChar(str: string): string | null {
+    for (let i = 0; i < str.length; i++) {
+        if (str.indexOf(str[i]) === str.lastIndexOf(str[i])) return str[i];
+    }
+    return null;
+}
